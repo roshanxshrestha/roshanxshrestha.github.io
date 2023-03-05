c391598e9bfb64ac6fe2090940c7c75c4f7a7b48
Roshan Shrestha <85292792+roshanxshrestha@users.noreply.github.com>
2023-03-05 13:57:00 +0545

    imageslides

--- script.js
+++ script.js
@@ -8,6 +8,23 @@ window.addEventListener("load", function () {
     AOS.init();
 
 });
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 5000); // Change image every 3 seconds
+}
+
+
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
