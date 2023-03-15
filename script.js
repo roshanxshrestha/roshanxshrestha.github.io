2b9d0b33d4ddc65b4a475ef5e51d64c81e952420
Roshan Shrestha <85292792+roshanxshrestha@users.noreply.github.com>
2023-03-15 16:38:07 +0545

    images

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 window.addEventListener("load", function () {
-    /*--------------------- page loader-----------*/
-    document.querySelector(".page-loader").classList.add("fade-out");
-    setTimeout(function () {
-        document.querySelector(".page-loader").style.display = "none";
-    }, 600);
-    /*------------------animation on scroll------------------------*/
-    AOS.init();
-
+  /*--------------------- page loader-----------*/
+  document.querySelector(".page-loader").classList.add("fade-out");
+  setTimeout(function () {
+    document.querySelector(".page-loader").style.display = "none";
+  }, 600);
+  /*------------------animation on scroll------------------------*/
+  AOS.init({
+    once: true,
+  });
 });
 
 var slideIndex = 0;
@@ -16,17 +17,18 @@ function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  slides[slideIndex-1].style.display = "block";  
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
   setTimeout(showSlides, 5000); // Change image every 3 seconds
 }
 
-
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
+window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
@@ -34,63 +36,62 @@ window.onscroll = function() {
     document.getElementById("navbar").style.top = "-60px";
   }
   prevScrollpos = currentScrollPos;
-}
+};
 
 $(document).ready(function () {
-    $(window).scroll(function () {
-        if (this.scrollY > 20) {
-            $('.navbar').addClass("sticky");
-        } else {
-            $('.navbar').removeClass("sticky");
-        }
-    });
-    // toggle menu/navbar
-    $('.menu-btn').click(function () {
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+  });
+  // toggle menu/navbar
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
 
-    // typing animation script
-    var typed = new Typed(".typing", {
-        strings: ["Learner", "App Developer", " Web Developer", "Technophile"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-    var typed = new Typed(".typing-2", {
-        strings: ["Learner", "App Developer", " Web Developer", "Technophile"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-    // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplayTimeout: 7000,
-        autoplayHoverPause: true,
-        responsive: {
-            0: {
-                items: 1,
-                nav: false
-            },
-            600: {
-                items: 2,
-                nav: false
-            },
-            1000: {
-                items: 3,
-                nav: false
-            }
-        }
-
-    });
-    // animation on scroll
-    AOS.init();
+  // typing animation script
+  var typed = new Typed(".typing", {
+    strings: ["Learner", "App Developer", " Web Developer", "Technophile"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+  var typed = new Typed(".typing-2", {
+    strings: ["Learner", "App Developer", " Web Developer", "Technophile"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+  // owl carousel script
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeout: 7000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
+  });
+  // animation on scroll
+  AOS.init();
 });
 /*-----------------close nav when clicking on a  nav item------------*/
 document.addEventListener("click", function (e) {
-    if (e.target.closest(".menu")) {
-        toggleNav();
-    }
+  if (e.target.closest(".menu")) {
+    toggleNav();
+  }
 });
