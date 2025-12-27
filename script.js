0e34ef3760981b5bd91842c73db9fff17f98bf05
Roshan Shrestha <85292792+roshanxshrestha@users.noreply.github.com>
2025-12-27 16:32:51 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -54,13 +54,25 @@ $(document).ready(function () {
 
   // typing animation script
   var typed = new Typed(".typing", {
-    strings: ["Learner", "App Developer", " Web Developer", "Technophile"],
+    strings: [
+      "Aspiring Data Analyst",
+      "Learner",
+      "App Developer",
+      " Web Developer",
+      "Technophile",
+    ],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
   });
   var typed = new Typed(".typing-2", {
-    strings: ["Learner", "App Developer", " Web Developer", "Technophile"],
+    strings: [
+      "Aspiring Data Analyst",
+      "Learner",
+      "App Developer",
+      " Web Developer",
+      "Technophile",
+    ],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
